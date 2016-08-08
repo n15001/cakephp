d278a600a32aaf47805bc71947bd196334cc6e69
n15001 <n15001@std.it-college.ac.jp>
2016-08-08 10:32:18 +0900

    コントローラーとビュー (6/6)

--- src/Controller/HeloController.php
+++ src/Controller/HeloController.php
@@ -5,29 +5,10 @@ use App\Controller\AppController;
  
 class HeloController extends AppController
 {
-    public function index($a = '')
+    public function index()
     {
-        //$this->set('message', 'Hello! this is sample page. ;-)');
-        if ($a == '') {
-            $this->setAction('err');
-            return;
-        }
-        $this->autoRender = false;
-        echo "<html><head></head><body>";
-        echo "<h1>Hello!</h1>";
-        echo "<p>これは、サンプルで作成したページです。</p><p>";
-
-	echo " パラメータ: " . $a;
-
-        echo "</p></body></html>";
-    }
-
-    public function err()
-    {
-        $this->autoRender = false;
-        echo "<html><head></head><body>";
-        echo "<h1>Hello!</h1>";
-        echo "<p>パラメータがありませんでした。</p>";
-        echo "</body></html>";
+        $id = $this->request->query('id');
+        $name = $this->request->query('name');
+        $this->set('message', 'your id:' . $id . ', name:' . $name);
     }
 }

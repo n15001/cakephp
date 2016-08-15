c084b3e03e3b963471f516df511d14b147552776
n15001 <n15001@std.it-college.ac.jp>
2016-08-15 12:24:31 +0900

    CRUDの基本について (4/9)

--- src/Controller/PersonsController.php
+++ src/Controller/PersonsController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 use App\Controller\AppController;
  
 class PersonsController extends AppController
-{ 
+{
+    public function index()
+    {
+        $this->set('persons', $this->Persons->find('all'));
+        }
+
     public function add()
     {
         if ($this->request->is('post')) {
